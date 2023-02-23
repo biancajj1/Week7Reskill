8f53ce6f655f70d64d1f6ca6b15ab80d0912e66b
biancajj1 <biancajj1@gmail.com>
2023-02-22 22:46:10 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+//import your http
+const http = require ("http")
+
+
+// create server with HTTP
+const server = http.createServer((req, res)=> {
+    console.log("server is created.")
+})
+
+//Initial Port
+
+const PORT= 4000;
+
+//Listen to Port
+server.listen(PORT, ()=> console.log(PORT))
